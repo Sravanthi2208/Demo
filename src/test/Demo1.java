cc58269387e08e1c8616ee058c55e31400bc8560
Sravanthi <Sravanthi@LAPTOP-A526OSJ3>
2023-03-29 13:32:11 +0530

    new  change added

--- src/test/Demo1.java
+++ src/test/Demo1.java
@@ -1,10 +1,11 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver;
 	}
 
 }
